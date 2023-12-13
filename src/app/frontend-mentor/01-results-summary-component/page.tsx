6fab2ac317949a817b8cd1a19c952a5e1e47a87d
Davide <myearthlydelight@gmail.com>
2023-12-13 17:22:08 +0100

    Changed component route, added font with Next.js localFont

--- src/app/frontend-mentor/01-results-summary-component/page.tsx
+++ src/app/frontend-mentor/01-results-summary-component/page.tsx
@@ -1,10 +1,33 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
+import localFont from "next/font/local";
+
 import styles from "./styles.module.css";
 import data from "./data.json";
 
+import memoryIcon from "./icon-memory.svg";
+import reactionIcon from "./icon-reaction.svg";
+import verbalIcon from "./icon-verbal.svg";
+import visualIcon from "./icon-visual.svg";
+
+interface IconType {
+  [key: string]: StaticImageData;
+}
+
+const icons: IconType = {
+  Reaction: reactionIcon,
+  Memory: memoryIcon,
+  Verbal: verbalIcon,
+  Visual: visualIcon,
+};
+
+const myFont = localFont({
+  src: "./HankenGrotesk-VariableFont_wght.ttf",
+  display: "swap",
+});
+
 export default function Page() {
   return (
-    <main className={styles.main}>
+    <main className={[styles.main, myFont.className].join(" ")}>
       <div className={styles.container}>
         <section className={styles.result}>
           <h1 className={styles.title}>Your Result</h1>
@@ -25,7 +48,7 @@ export default function Page() {
                 className={[styles["break-item"], styles[`break-item-${item.category.toLowerCase()}`]].join(" ")}
                 key={item.category}
               >
-                <Image src={item.icon} alt="" />
+                <Image src={icons[item.category]} alt="" />
                 <p className={styles["break-item-name"]}>{item.category}</p>
                 <p className={styles["break-item-score"]}>{item.score}</p>
                 <p className={styles["break-item-total"]}>&nbsp;&nbsp;/ 100</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen p-24">
       <h1 className="mb-4 text-3xl">Frontend Mentor</h1>
-      <Link href="/frontend-mentor/01-results-summary">01 Results Summary Component</Link>
+      <Link href="/frontend-mentor/01-results-summary-component">01 Results Summary Component</Link>
     </main>
   );
 }
