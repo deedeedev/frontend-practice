7be418fdd24b4f4af453185fd9687ec824da06e6
Davide <myearthlydelight@gmail.com>
2023-12-14 15:51:18 +0100

    Refactoring: created QRCode component

--- src/app/frontend-mentor/02-qr-code-component/page.tsx
+++ src/app/frontend-mentor/02-qr-code-component/page.tsx
@@ -1,24 +1,6 @@
-import Image from "next/image";
-import { Outfit } from "next/font/google";
-
-import styles from "./styles.module.css";
+import QRCode from "@/components/QRCode/QRCode";
 import qrCodeImage from "./image-qr-code.png";
 
-const outfitFont = Outfit({
-  subsets: ["latin"],
-  display: "swap",
-});
-
 export default function Page() {
-  return (
-    <div className={[styles.container, outfitFont.className].join(" ")}>
-      <Image src={qrCodeImage} className={styles.qrcode} alt="" />
-      <div className={styles.textContainer}>
-        <h1 className={styles.title}>Improve your front-end skills by building projects</h1>
-        <p className={styles.text}>
-          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
-        </p>
-      </div>
-    </div>
-  );
+  return <QRCode image={qrCodeImage} />;
 }
