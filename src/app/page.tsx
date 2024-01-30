96392b5ea0ef9bb1b46203c54896b7bfb25cc4b6
Davide <myearthlydelight@gmail.com>
2024-01-30 20:58:22 +0100

    Moved home styles out of root layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import "./styles.css";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen p-24">
@@ -7,6 +9,7 @@ export default function Home() {
       <Link href="/frontend-mentor/01-results-summary-component">01 Results Summary Component</Link>
       <Link href="/frontend-mentor/02-qr-code-component">02 QR Code Component</Link>
       <Link href="/frontend-mentor/03-3-column-preview-card-component">03 3 Column Preview Card</Link>
+      <Link href="/frontend-mentor/04-recipe-page">04 Recipe Page</Link>
     </main>
   );
 }
