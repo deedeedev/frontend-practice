91ea311ef60e58caa0555d43acf6fd590d2bd73f
Davide <myearthlydelight@gmail.com>
2024-02-17 17:12:13 +0100

    Four Card Feature Section: challenge completed

--- src/app/frontend-mentor/(pages)/13-four-card-feature-section/page.tsx
+++ src/app/frontend-mentor/(pages)/13-four-card-feature-section/page.tsx
@@ -1,28 +1,72 @@
 import Image from "next/image"
 import { Poppins } from "next/font/google"
 import FeatureCard from "@/components/FeatureCard/FeatureCard"
+import clsx from "clsx"
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["200", "400", "600"] })
 
+const features = [
+  {
+    title: "Supervisor",
+    text: "Monitors activity to identify project roadblocks",
+    color: "hsl(180, 62%, 55%)",
+    icon: "/13-four-card-feature-section/icon-supervisor.svg",
+  },
+  {
+    title: "Team Builder",
+    text: "Scans our talent network to create the optimal team for your project",
+    color: "hsl(0, 78%, 62%)",
+    icon: "/13-four-card-feature-section/icon-team-builder.svg",
+  },
+  {
+    title: "Karma",
+    text: "Regularly evaluates our talent to ensure quality",
+    color: "hsl(34, 97%, 64%)",
+    icon: "/13-four-card-feature-section/icon-karma.svg",
+  },
+  {
+    title: "Calculator",
+    text: "Uses data from past projects to provide better delivery estimates",
+    color: "hsl(212, 86%, 64%)",
+    icon: "/13-four-card-feature-section/icon-calculator.svg",
+  },
+]
+
 export default function Page() {
   return (
-    <div
-      className={`${poppins.className} bg-scfsVeryLightGray text-scfsGrayishBlue flex min-h-screen w-full items-center justify-center overflow-hidden text-[15px]`}
+    <section
+      className={`${poppins.className} bg-scfsVeryLightGray text-scfsGrayishBlue flex min-h-screen w-full items-center justify-center overflow-hidden px-8 py-20 text-[15px]`}
     >
-      <div className="container">
-        <div className="text-center">
-          <p className="text-2xl font-extralight">
+      {/* Content */}
+      <div className="flex flex-col gap-16">
+        {/* Intro */}
+        <div className="mx-auto max-w-[315px] text-center md:max-w-lg">
+          <p className="text-scfsVeryDarkBlue text-2xl font-light md:text-4xl">
             Reliable, efficient delivery
           </p>
-          <h1 className="text-scfsVeryDarkBlue mb-5 mt-2 text-2xl font-bold">
+          <h1 className="text-scfsVeryDarkBlue mb-5 mt-2 text-2xl font-bold md:text-4xl">
             Powered by Technology
           </h1>
-          <p className="">
+          <p className="text-[16px]">
             Our Artificial Intelligence powered tools use millions of project
             data points to ensure that your project is successful
           </p>
         </div>
+        {/* Cards */}
+        <div className="grid items-center justify-center gap-6 md:grid-cols-3 md:grid-rows-2">
+          {features.map((feature, i) => (
+            <div
+              key={i}
+              className={clsx({
+                "md:row-span-full": i === 0 || i === 3,
+                "md:col-start-3": i === 3,
+              })}
+            >
+              <FeatureCard {...feature} />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
--- src/components/FeatureCard/FeatureCard.tsx
+++ src/components/FeatureCard/FeatureCard.tsx
@@ -1,3 +1,29 @@
-export default function FeatureCard() {
-  return <div>Hello, World!</div>
+import { StaticImport } from "next/dist/shared/lib/get-img-props"
+import Image from "next/image"
+
+interface FeatureCardProps {
+  title: string
+  text: string
+  color: string
+  icon: string
+}
+
+export default function FeatureCard({
+  title,
+  text,
+  color,
+  icon,
+}: FeatureCardProps) {
+  return (
+    <div
+      className="flex min-h-[250px] max-w-[350px] flex-col gap-2 rounded-lg p-8 shadow-xl"
+      style={{ borderTop: `4px solid ${color}` }}
+    >
+      <h1 className="text-scfsVeryDarkBlue text-xl font-bold">{title}</h1>
+      <p className="text-[13px]">{text}</p>
+      <div className="mt-auto self-end">
+        <Image src={icon} width={65} height={65} alt="title" />
+      </div>
+    </div>
+  )
 }
