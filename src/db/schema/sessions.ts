d6d4a9b79810f03fdbadf3adfe206fb23d0b8726
Davide <myearthlydelight@gmail.com>
2024-03-09 17:48:30 +0100

    Created sessions table schema

---
+++ src/db/schema/sessions.ts
@@ -0,0 +1,20 @@
+import { sql } from "drizzle-orm"
+import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core"
+
+export const sessions = sqliteTable("sessions", {
+  id: integer("id", { mode: "number" })
+    .notNull()
+    .primaryKey({ autoIncrement: true }),
+  userId: integer("user_id", { mode: "number" }).notNull(),
+  token: text("token").notNull(),
+  expiresAt: text("expires_at").notNull(),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: text("updated_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+})
+
+export type SelectSession = typeof sessions.$inferSelect
+export type InsertSession = typeof sessions.$inferInsert
